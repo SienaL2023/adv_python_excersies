435e37e1c4fbef3d1610ed32b71524f337265bdd
SienaL2023 <xiaosiena@gmail.com>
2024-02-18 18:18:57 -0800

    worked on common prefix

--- main.py
+++ main.py
@@ -60,7 +60,22 @@ assert result2 == True
 # find the longest common prefix string amongst the list of strings
 
 def longestCommonPrefix(strings): # return the longest prefix string
-    pass
+    shortest = 9999
+    for x in strings:
+        if len(x)< shortest:
+            shortest = len(x)
+    # by here shortest word length is found
+    prefix = ""
+    for y in range(shortest):
+        letter_y = strings[0][y]
+
+        for x in range(len(strings)):
+            print(strings[x][y])
+            temp = strings[x][y]
+            if temp != letter_y:
+                return prefix
+        prefix += letter_y
+
     # create empty string
     # go through each character in shortest word -->  for loop
     # go through each word in list --> for loop
@@ -68,7 +83,19 @@ def longestCommonPrefix(strings): # return the longest prefix string
     # if not: return string (prefix)
     # if so: add to empty string
 
+    # more optimal solution
+    # 1. sort the lust of strings alphabetically
+    # 2. check first and last word fro longest common prefix
+    # 3. return longest
+
 strings = ["hello", "help", "hell", "heeeee", "heel", "hi"]
 print(strings[0][0]) # gets first letter of hello
 print(strings[1][0])
 print(strings[2][0])
+
+
+
+
+
+# solution = longestCommonPrefix(["hello", "help", "hell", "heeeee", "heel", "hi"])
+# assert solution == "h"
