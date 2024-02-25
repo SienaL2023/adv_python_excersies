449ff8cf9f546fed5d47521072dd0b99a9338df5
SienaL2023 <xiaosiena@gmail.com>
2024-02-25 11:02:10 -0800

    started stock function

--- main.py
+++ main.py
@@ -60,21 +60,29 @@ assert result2 == True
 # find the longest common prefix string amongst the list of strings
 
 def longestCommonPrefix(strings): # return the longest prefix string
-    shortest = 9999
-    for x in strings:
-        if len(x)< shortest:
-            shortest = len(x)
-    # by here shortest word length is found
-    prefix = ""
-    for y in range(shortest):
-        letter_y = strings[0][y]
+    # shortest = 9999
+    # for x in strings:
+    #     if len(x)< shortest:
+    #         shortest = len(x)
+    # # by here shortest word length is found
+    # prefix = ""
+    # for y in range(shortest):
+    #     letter_y = strings[1][y]
+    #
+    #     for x in range(len(strings)):
+    #         # print(strings[x][y])
+    #         temp = strings[x][y]
+    #         if temp != letter_y:
+    #             return prefix
+    #     prefix += letter_y
 
-        for x in range(len(strings)):
-            print(strings[x][y])
-            temp = strings[x][y]
-            if temp != letter_y:
-                return prefix
-        prefix += letter_y
+    strings.sort() # nlogn # sort alphabetically
+    prefix = "" # creates blank string to put letter of prefixes in
+    for x in range(len(strings[0])): # o(n) # for x in range of the len of the first letter
+        temp = strings[len(strings) - 1][x] # temp = the x letter in the last word of list
+        if temp != strings[0][x]: # compare temp and x letter of first word
+            return prefix # return gets u out of the function
+        prefix += temp # add letter
 
     # create empty string
     # go through each character in shortest word -->  for loop
@@ -84,18 +92,48 @@ def longestCommonPrefix(strings): # return the longest prefix string
     # if so: add to empty string
 
     # more optimal solution
-    # 1. sort the lust of strings alphabetically
+    # 1. sort the list of strings alphabetically list.sort()
     # 2. check first and last word fro longest common prefix
     # 3. return longest
 
-strings = ["hello", "help", "hell", "heeeee", "heel", "hi"]
-print(strings[0][0]) # gets first letter of hello
-print(strings[1][0])
-print(strings[2][0])
+# strings = ["hello", "help", "hell", "heeeee", "heel", "hi"]
+# print(strings[0][0]) # gets first letter of hello
+# print(strings[1][0])
+# print(strings[2][0])
 
+solution = longestCommonPrefix(["hello", "help", "hell", "heeeee", "heel", "hi"])
+assert solution == "h"
 
+solution = longestCommonPrefix(["nemo", "neighbor", "nick"])
+assert solution == "n"
 
+# exercise # 3
+# best time to buy/sell stock
+# input: list of integers --> price of that stock per day
+# output: find maximum profit where you will buy from the cheapest and sell at a later time
 
+def maxProfit(prices):
+    # profit = 0
+    # for each number in prices:
+    #   temp = number
+    #   for every number after temp:
+    #       get diff between number and temp
+    #       update profit variable
+    # return profit
+    profit = 0
+    for x in range(len(prices)):
+        temp = prices[x]
 
-# solution = longestCommonPrefix(["hello", "help", "hell", "heeeee", "heel", "hi"])
-# assert solution == "h"
+        for y in range(x+1, len(prices)):
+            diff = prices[y] - temp
+            if diff > profit:
+                profit = diff
+    return profit
+
+print(min(5,7))
+print(max(5,7))
+profit = maxProfit([7,1,5,3,6,4])
+assert profit == 5
+
+profit = maxProfit([7,6,4,3,1])
+assert profit == 0
